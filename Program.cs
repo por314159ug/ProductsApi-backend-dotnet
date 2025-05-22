d20e2ba4f744cba57503f28eae1900e6c11d828b
Piotr <por314ug@gmail.com>
2025-05-22 12:42:04 +0200

    Configure CORS

--- Program.cs
+++ Program.cs
@@ -3,19 +3,40 @@ using ProductsApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<ProductContext>(options =>
     options.UseInMemoryDatabase("Products"));
+
+// Konfiguracja CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+// Użycie CORS
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
